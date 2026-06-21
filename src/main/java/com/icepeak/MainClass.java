ccb2dc84130582c05a01b018856aac3af2ae71ec
IcepeakGr <darkmastergr2001@gmail.com>
2026-06-21 23:28:52 +0300

    Modify plugin.yml.
    Change the plugin.yml file to use the correct main class path.

--- src/main/java/com/icepeak/MainClass.java
+++ src/main/java/com/icepeak/MainClass.java
@@ -6,12 +6,16 @@ public class MainClass extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        getLogger().info("===================================");
         getLogger().info("TheLeftBehind has been enabled!");
+        getLogger().info("===================================");
     }
 
     @Override
     public void onDisable() {
+        getLogger().info("===================================");
         getLogger().info("TheLeftBehind has been disabled!");
+        getLogger().info("===================================");
     }
 
 }
